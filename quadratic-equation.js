9f28e74edef298a3961e79af828eaf735f4f2f36
Alexey Drobyashenko <mr.leon327@yandex.ru>
2013-12-03 19:17:10 +0400

    решил задачу

---
+++ quadratic-equation.js
@@ -0,0 +1,27 @@
+/**
+ * Created by leon on 03.12.13.
+ */
+var a;
+var b;
+var c;
+
+var d =(Math.pow(b,2)-(4*a*c));
+
+if (d>0)
+    {
+        if (d==0)
+            {
+                var x1 = (-b +Math.sqr(d)/(2*a));
+                console.log("x1 = x2 = "+x1);
+            }
+        else
+        {
+            var x1 = (-b + Math.sqrt(d))/(2*a);
+            var x2 = (-b - Math.sqrt((d)/(2*a)));
+            console.log("x1="+x1+"; x2= "+x2);
+        }
+    }
+else
+    {
+        console.log("Корней нет")
+    }
