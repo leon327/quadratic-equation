366adacff03f961b3348b2d3953ad77a24d95e5b
Alexey Drobyashenko <mr.leon327@yandex.ru>
2013-12-06 19:46:37 +0400

    fixed #1 fixed2 fixed#3

--- quadratic-equation.js
+++ quadratic-equation.js
@@ -1,33 +1,25 @@
 /**
  * Created by leon on 03.12.13.
  */
-var a;
-var b;
-var c;
-
-if (a != null && b != null && c != null)
-{
+  function quadratic(a, b, c){
+if (typeof(a) === 'number' &&
+    typeof(b) === 'number' &&
+    typeof(c) === 'number'){
   var d = Math.pow(b, 2) - (4 * a * c);
-  if (d >= 0)
-  {
-    if (d == 0)
-    {
-      var x1 = (- b + Math.sqr(d) / (2 * a));
+  if (d >= 0){
+    var x1;
+    var x2;
+    if (d == 0){
+       x1 = x2 = (- b + Math.sqrt(d) / (2 * a));
       console.log('x1 = x2 = ' + x1);
-    }
-    else
-    {
-      var x1 = (- b + Math.sqrt(d)) / (2 * a);
-      var x2 = (- b - Math.sqrt(d)) / (2 * a);
-      console.log('x1 = ' + x1 + '; x2 = ' + x2);
-    }
-  }
-  else
-  {
-    console.log('Корней нет');
-  }
-}
-else
-{
+    }else{
+       x1 = (- b + Math.sqrt(d)) / (2 * a);
+       x2 = (- b - Math.sqrt(d)) / (2 * a);
+      console.log('x1 = ' + x1 + '; x2 = ' + x2);}
+  }else{
+    console.log('Корней нет');}
+}else{
   console.log('Введите переменные');
+  }
 }
+quadratic(2, 4, 8);
